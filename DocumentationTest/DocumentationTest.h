0e873fd75ea59fcbe2ad834d80798da638395eee
generic-keith <keith.staines.ext@bbc.co.uk>
2023-08-31 06:17:05 +0100

    Initial Commit

---
+++ DocumentationTest/DocumentationTest.h
@@ -0,0 +1,18 @@
+//
+//  DocumentationTest.h
+//  DocumentationTest
+//
+//  Created by Keith Staines on 31/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocumentationTest.
+FOUNDATION_EXPORT double DocumentationTestVersionNumber;
+
+//! Project version string for DocumentationTest.
+FOUNDATION_EXPORT const unsigned char DocumentationTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentationTest/PublicHeader.h>
+
+
